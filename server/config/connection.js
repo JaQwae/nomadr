00560aeb4beab5e154b1038a33010d4c35655193
JaQwae <105896967+JaQwae@users.noreply.github.com>
2022-11-28 19:48:48 -0600

    graphql set up

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,12 +1,19 @@
 const mongoose = require("mongoose");
 
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/googlebooks', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
+});
 // Node will look for this environment variable and if it exists, it will use it. Otherwise, it will assume that you are running this application locally
-mongoose.connect(
-  process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/socialDB",
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  }
-);
+// mongoose.connect(
+//   process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/socialDB",
+//   {
+//     useNewUrlParser: true,
+//     useUnifiedTopology: true,
+//   }
+// );
 
 module.exports = mongoose.connection;
