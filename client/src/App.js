574d886ba6dc0535aff7152a24b8f619a3107a43
alexyn26 <107084291+alexyn26@users.noreply.github.com>
2022-11-29 20:10:46 -0600

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
 import React, {useState} from 'react';
 import FlashcardList from './LanguageCards/FlashcardList';
 import {spanishList} from './LanguageCards/spanishDB';
 import {frenchList} from './LanguageCards/frenchDB';
-import {BrowerRouter as Router, Route} from 'react-router-dom';
-import NavbarOutline from './NavbarOutline;'
+import {BrowserRouter as Router, route } from "react-router-dom";
 
+import NavbarOutline from './NavbarOutline';
 function App() {
   const [cards, setCards] = useState(frenchList)
   return (
