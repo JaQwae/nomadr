72ae1a0280f2a81d11a8c69986812ba882827991
alexyn26 <107084291+alexyn26@users.noreply.github.com>
2022-11-29 19:39:50 -0600

    Create frenchDB.js
    adding id's and changing values

---
+++ client/src/LanguageCards/frenchDB.js
@@ -0,0 +1,158 @@
+
+
+export const frenchList = 
+[
+    {
+        "id":1,      
+        "englishWord": "Hello",
+        "translation": "bonjour"
+        
+      },
+      {
+        "id":2,   
+        "englishWord": "Good Afternoon",
+        "translation": "bonne après-midi"
+        
+      },
+      {
+        "id":3,   
+        "englishWord": "Good Night",
+        "translation": "bonne nuit"
+        
+      },
+      {
+        "id":4,   
+        "englishWord": "see you later",
+        "translation": "à plus tard"
+        
+      },
+      {
+        "id":5,   
+        "englishWord": "please",
+        "translation": "s'il vous plaît"
+        
+      },
+      {
+        "id":6,   
+        "englishWord": "thank you",
+        "translation": "merci"
+        
+      },
+      {
+        "id":7,   
+        "englishWord": "I'm sorry",
+        "translation": "Je suis désolé"
+        
+      },
+      {
+        "id":8,   
+        "englishWord": "excuse me",
+        "translation": "pardon"
+        
+      },
+      {
+        "id":9,   
+        "englishWord": "yes",
+        "translation": "oui"
+        
+      },
+      {
+        "id":10,   
+        "englishWord": "no",
+        "translation": "non"
+        
+      },
+      {
+        "id":11,   
+        "englishWord": "I don't understand",
+        "translation": "je ne comprends pas"
+        
+      },
+      {
+        "id":12,   
+        "englishWord": "Do you speak english?",
+        "translation": "parlez vous anglais?"
+        
+      },
+      {
+        "id":13,   
+        "englishWord": "How are you?",
+        "translation": "Comment vas-tu?"
+        
+      },
+      {
+        "id":14,   
+        "englishWord": "What is this?",
+        "translation": "Qu'est-ce que c'est?"
+        
+      },
+      {
+        "id":15,   
+        "englishWord": "How much does this cost?",
+        "translation": "Combien ça coûte?"
+        
+      },
+      {
+        "id":16,   
+        "englishWord": "Where is...?",
+        "translation": "où est...?"
+        
+      },
+      {
+        "id":17,   
+        "englishWord": "I want....",
+        "translation": "Je veux..."
+        
+      },
+      {
+        "id":18,   
+        "englishWord": "Do you have...?",
+        "translation": "As-tu...?"
+        
+      },
+      {
+        "id":19,   
+        "englishWord": "I am lost",
+        "translation": "je suis perdu"
+        
+      },
+      {
+        "id":20,   
+        "englishWord": "Is it ok to drink the tap water?",
+        "translation": "Est-il acceptable de boire l'eau du robinet ?"
+        
+      },
+      {
+        "id":21,   
+        "englishWord": "How do I get to...",
+        "translation": "Comment puis-je aller à...?"
+        
+      },
+      {
+        "id":22,   
+        "englishWord": "Can you help me?",
+        "translation": "Pouvez-vous m'aider?"
+        
+      },
+      {
+        "id":23,   
+        "englishWord": "I am sick",
+        "translation": "je suis malade"
+        
+      },
+      {
+        "id":24,   
+        "englishWord": "left",
+        "translation": "la gauche"
+        
+      },
+      {
+        "id":25,   
+        "englishWord": "right",
+        "translation": "droit"
+        
+      },
+     
+
+
+]
