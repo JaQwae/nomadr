bb7d4dfe6e52fb64b1212ef06ea5a10f03060a2c
alexyn26 <107084291+alexyn26@users.noreply.github.com>
2022-11-29 19:38:56 -0600

    Update App.js
    importing files and rendering flashcards

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,21 @@
 import logo from './logo.svg';
 import './App.css';
-import React from 'react';
+import React, {useState} from 'react';
+
+
+import FlashcardList from './LanguageCards/FlashcardList';
+import {spanishList} from './LanguageCards/spanishDB';
+import {frenchList} from './LanguageCards/frenchDB';
 
 function App() {
+  const [cards, setCards] = useState(frenchList)
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    
+    <div>
+      
+     
+
+      <FlashcardList flashcards = {cards}/>
     </div>
   );
 }
