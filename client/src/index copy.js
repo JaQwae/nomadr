7b2e070ed107dd280c93a60e078e5f17cc9c5721
Cindy Paredes <cindyrangel@Cindys-iMac.local>
2022-11-29 18:57:16 -0600

    added the navbar

---
+++ client/src/index copy.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import ReactDOM from 'react-dom';
+
+
+
+import NavbarOutline from './NavbarOutline';
+
+ReactDOM.render(<NavbarOutline />, document.getElementById('root'));
+
+  
+
