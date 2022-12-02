2861dc7a9007749885f06785389cc81b399bd2ac
jlewisit <joelewis.mortgage@gmail.com>
2022-12-01 18:03:00 -0800

    react restructuring mostly complete

---
+++ client/src/NomadVisas/index.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { useLocation, useNavigate } from 'react-router-dom';
+// import { Country } from '../../server/schema/typeDefs';
+// import { Income } from '../../server/schema/typeDefs';
+const Income = require ('../../server/schema/typeDefs');
+const Country = require ('../../server/schema/typeDefs');
+
+
+const Visas = () => {
+  const location = useLocation();
+  const navigate = useNavigate();
+  return (
+    <div className="w-100 mt-auto bg-secondary p-4">
+      <div className="container text-center mb-5">
+        { Country } {Income };
+        {location.pathname !== '/' && (
+          <button
+            className="btn btn-dark mb-3"
+            onClick={() => navigate(-1)}
+          >
+            &larr; Go Back
+          </button>
+        )}
+        <h4>
+        </h4>
+      </div>
+    </div>
+  );
+};
+
+export default Visas;
--- client/src/components/Pages/Home.js
+++ client/src/components/Pages/Home.js
@@ -1,5 +1,4 @@
-import React, {useState} from 'react'
-
+import React from 'react'
 
 function Home() {
 
