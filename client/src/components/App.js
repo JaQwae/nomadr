113b826d6e77b03e039ef093d4fa71021d9d4dc2
alexyn26 <107084291+alexyn26@users.noreply.github.com>
2022-12-01 18:06:59 -0600

    Update App.js
    routing

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -2,13 +2,17 @@ import React from 'react';
 import {BrowerRouter as Router, Route } from "react-router-dom";
 import NavbarOutline from "./Navbar/Navbar";
 import PlanTrip from "./PlanTrip"
+import LanguagePractice from './Pages/LanguagePractice';
+import Home from './Pages/Home';
 
 function App() {
     return(
         <Router> 
         <div>
             <NavbarOutline/>
-            <Route exact path="/home" component={PlanTrip} />
+            <Route exact path="/home" component={Home} />
+            <Route exact path="/languagepractice" component={LanguagePractice} />
+
         </div>
         </Router>
     )
