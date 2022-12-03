3a5cc8e02d334aef1fdd8bd2d4e74de93c72c769
jlewisit <joelewis.mortgage@gmail.com>
2022-12-03 11:46:45 -0800

    Cart index and style

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -2,6 +2,7 @@ const { Country } = require("../models");
 const { AuthenticationError } = require("apollo-server-express");
 const { User } = require("../models");
 const { signToken } = require("../utils/auth");
+const stripe = require('stripe')('sk_test_Gx4mWEgHtCMr4DYMUIqfIrsz');
 
 const resolvers = {
   Query: {
@@ -21,21 +22,56 @@ const resolvers = {
       }
       throw new AuthenticationError("You need to be logged in!");
     },
-  },
+    checkout: async (parent, args, context) => {
+      const url = new URL(context.headers.referer).origin;
+      const order = new Order({ products: args.products });
+      const line_items = [];
 
+      const { products } = await order.populate('products');
+
+      for (let i = 0; i < products.length; i++) {
+        const product = await stripe.products.create({
+          name: products[i].name,
+          // description: products[i].description,
+          // images: [`${url}/images/${products[i].image}`]
+        });
+
+        const price = await stripe.prices.create({
+          product: product.id,
+          unit_amount: products[i].price * 100,
+          currency: 'usd',
+        });
+
+        line_items.push({
+          price: price.id,
+          quantity: 1
+        });
+      }
+
+      const session = await stripe.checkout.sessions.create({
+        payment_method_types: ['card'],
+        line_items,
+        mode: 'payment',
+        success_url: `${url}/success?session_id={CHECKOUT_SESSION_ID}`,
+        cancel_url: `${url}/`
+      });
+
+      return { session: session.id };
+    }
+  },
   Mutation: {
     createMatchup: async (parent, args) => {
       const matchup = await Matchup.create(args);
       return matchup;
     },
-    createVote: async (parent, { _id, techNum }) => {
-      const vote = await Matchup.findOneAndUpdate(
-        { _id },
-        { $inc: { [`tech${techNum}_votes`]: 1 } },
-        { new: true }
-      );
-      return vote;
-    },
+    // createVote: async (parent, { _id, techNum }) => {
+    //   const vote = await Matchup.findOneAndUpdate(
+    //     { _id },
+    //     { $inc: { [`tech${techNum}_votes`]: 1 } },
+    //     { new: true }
+    //   );
+    //   return vote;
+    // },
     addUser: async (parent, { username, email, password }) => {
       const user = await User.create({ username, email, password });
       const token = signToken(user);
