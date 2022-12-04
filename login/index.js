f03375abcb1ff433f4ca9a8772232c0bc084bef6
Collins418 <106499144+Collins418@users.noreply.github.com>
2022-12-03 18:57:20 -0600

    updated register with all the fixins'

---
+++ login/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import { AuthProvider } from './AuthProvider';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <AuthProvider>
+      <App />
+    </AuthProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
---
+++ signin-register/app.js
@@ -0,0 +1,14 @@
+import Register from './Register';
+import Login from '../Login/login';
+
+function App() {
+
+  return (
+    <main className="App">
+      <Register />
+      <Login />
+    </main>
+  );
+}
+
+export default App;
