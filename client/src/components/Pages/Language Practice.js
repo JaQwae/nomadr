570ab9f563d169e8ea64bf70c1b0aa91ecfbf9c2
alexyn26 <107084291+alexyn26@users.noreply.github.com>
2022-12-05 19:50:47 -0600

    Update Language Practice.js
    adding all languages

--- client/src/components/Pages/Language Practice.js
+++ client/src/components/Pages/Language Practice.js
@@ -1,19 +1,38 @@
 import React, {useState} from 'react'
 import {frenchList} from '../LanguageCards/frenchDB';
-import{spanishList} from '../LanguageCards/spanishDB'
+import{spanishList} from '../LanguageCards/spanishDB';
+import {portugueseList} from '../LanguageCards/portugueseDB';
 import FlashcardList from '../LanguageCards/FlashcardList';
 
 function LanguagePractice() {
 
-    const [cards, setCards] = useState(frenchList)
+    const [spanishcards] = useState(spanishList)
+    const [frenchcards] = useState(frenchList)
+    const [portuguesecards] = useState(portugueseList)
 
    
   return (
    
 <div>
 <h1>Lets Practice the Language!</h1>
-      <FlashcardList flashcards = {cards}/>
 
+<div class = 'spanish'>
+  <h2>Spanish</h2>
+  
+      <FlashcardList flashcards = {spanishcards}/>
+</div>
+
+<div class = 'french'>
+  <h2>French</h2>
+  
+      <FlashcardList flashcards = {frenchcards}/>
+</div>
+
+<div class = 'portuguese'>
+  <h2>Portuguese</h2>
+  
+      <FlashcardList flashcards = {portuguesecards}/>
+</div>
     </div>
   )
 }
