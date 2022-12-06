c33d917085ea105b2e6d50900abe26856965f990
alexyn26 <107084291+alexyn26@users.noreply.github.com>
2022-12-05 19:05:41 -0600

    Update Language Practice.js
    adding in language

--- client/src/components/Pages/Language Practice.js
+++ client/src/components/Pages/Language Practice.js
@@ -1,15 +1,17 @@
 import React, {useState} from 'react'
 import {frenchList} from '../LanguageCards/frenchDB';
+import{spanishList} from '../LanguageCards/spanishDB'
 import FlashcardList from '../LanguageCards/FlashcardList';
 
-
 function LanguagePractice() {
+
     const [cards, setCards] = useState(frenchList)
 
+   
   return (
-    <div>
-      <h1>Lets Practice the Language!</h1>
-
+   
+<div>
+<h1>Lets Practice the Language!</h1>
       <FlashcardList flashcards = {cards}/>
 
     </div>
