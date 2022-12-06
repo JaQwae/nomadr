a2e125b5877b9e0dfab027bfddd94f1ab13dd684
alexyn26 <107084291+alexyn26@users.noreply.github.com>
2022-12-05 19:06:20 -0600

    Create Resources.js
    adding in resources

---
+++ client/src/components/Pages/Resources.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Resources() {
+    return(
+        <div className = 'Resources'>
+            <h1>Switch successful</h1>
+        </div>
+    )
+}
+
+export default Resources;
