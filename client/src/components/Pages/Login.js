248bc3e7db65583f0345c4484021b4f18b780d0f
alexyn26 <107084291+alexyn26@users.noreply.github.com>
2022-12-05 20:54:17 -0600

    Update Login.js
    styling login

--- client/src/components/Pages/Login.js
+++ client/src/components/Pages/Login.js
@@ -12,13 +12,14 @@ import React, { useState } from "react";
         <div className="auth-form-container">
             <h2>Login</h2>
         <form className="login-form" onSubmit={handleSubmit}>
-            <label htmlFor="email">Email</label>
-            <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="example@email.com" id="email" name="email" />
-            <label htmlFor="password">Password</label>
-            <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="*********" id="password" name="password" />
-            <button type="submit">Log In</button>
-        </form>
+            <label htmlFor="email">Email</label><br></br>
+            <input value={email} onChange={(e) => setEmail(e.target.value)} type="email" placeholder="example@email.com" id="email" name="email" /><br></br>
+            <label htmlFor="password">Password</label><br></br>
+            <input value={pass} onChange={(e) => setPass(e.target.value)} type="password" placeholder="*********" id="password" name="password" /><br></br>
+            <button type="submit">Log In</button><br></br>
+        
         <button className="link-btn" onClick={()=> props.onFormSwitch('register')}>Don't have an account? Register here</button>
+        </form>
         </div>
     )
 }
