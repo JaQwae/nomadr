82ab69c092af39f2a0a77982ad2a63287b7521eb
alexyn26 <107084291+alexyn26@users.noreply.github.com>
2022-12-05 20:54:32 -0600

    Update Resources.js
    adding resource links

--- client/src/components/Pages/Resources.js
+++ client/src/components/Pages/Resources.js
@@ -7,6 +7,15 @@ function Resources() {
             <div class = "Links">
                 <a href = "https://www.skyscanner.com/" target="_blank">Sky Scanner</a>
             </div>
+            <div class = "Links">
+                <a href = "https://www.expedia.com/" target="_blank">Expedia</a>
+            </div>
+            <div class = "Links">
+                <a href = "https://www.trivago.com/" target="_blank">Trivago</a>
+            </div>
+            <div class = "Links">
+                <a href = "https://www.booking.com/" target="_blank">Booking.com</a>
+            </div>
         </div>
     )
 }
