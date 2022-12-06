2b98931701bf2e7224d537fe2c500838ec2fae35
alexyn26 <107084291+alexyn26@users.noreply.github.com>
2022-12-05 19:52:05 -0600

    Update Resources.js
    adding resource section

--- client/src/components/Pages/Resources.js
+++ client/src/components/Pages/Resources.js
@@ -3,7 +3,10 @@ import React from 'react'
 function Resources() {
     return(
         <div className = 'Resources'>
-            <h1>Switch successful</h1>
+            <h1>Complimentary Travel Sites</h1>
+            <div class = "Links">
+                <a href = "https://www.skyscanner.com/" target="_blank">Sky Scanner</a>
+            </div>
         </div>
     )
 }
