b362eb448e2ac22e8be989418826f1482edc0bc0
alexyn26 <107084291+alexyn26@users.noreply.github.com>
2022-12-05 22:41:22 -0600

    Update Login.js
    login page style

--- client/src/components/Pages/Login.js
+++ client/src/components/Pages/Login.js
@@ -9,7 +9,53 @@ import React, { useState } from "react";
         console.log(email);
     }
     return(
-        <div className="auth-form-container">
+
+
+        <div className = "Home">
+        <br></br>
+      <h1>A New Sky, A New Life.</h1>
+     
+      <div className = "grid-container1">
+        <div className = "grid-item1">
+      <img src = "https://i.natgeofe.com/n/6c02ad5a-977b-4f12-b9c0-02ffb0736e07/metropolitan-cathedral-zocalo-mexico-city_16x9.JPG " alt= "Mexico"></img>
+      </div>
+      <div className = "grid-item1">
+    <img src = "https://www.state.gov/wp-content/uploads/2018/11/France-1980x1406.jpg" alt ="France"></img>
+    </div>
+    <div className = "grid-item1">
+    <img src = "https://i.natgeofe.com/k/33e48abd-f2e7-4430-b7bf-cc9a18c14cc6/brazil-christ-the-redeemer_2x1.jpg" alt = "Brazil"></img>
+    </div>
+    <div className = "grid-item1">
+    <img src = "https://www.state.gov/wp-content/uploads/2018/11/Spain-2109x1406.jpg" alt = "Spain"></img>
+   </div>
+   <div className = "grid-item1">
+    <img src = "https://www.travelandleisure.com/thmb/CwASoPDthL_sRQW6yA-qh-zJPlg=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/santorini-POINTSGREECE0617-8cd08e70fec4471b8827dca8627b8eae.jpg" alt = "greece"></img>
+   </div>
+   <div className = "grid-item1">
+    <img src =  "https://images.ireland.com/media/Images/Tipperary/aafe8401d6a1418cb65e2767f330c94b.jpg?w=1934" alt = "Ireland"></img>
+   </div>
+   <div className = "grid-item1">
+    <img src =  "https://i.natgeofe.com/k/42e832f5-fd48-43ff-b338-091bdf4048ca/india-tajmahal_16x9.jpg?w=1200" alt = "India"></img>
+    </div>
+    <div className = "grid-item1">
+    <img src = "https://a.cdn-hotels.com/gdcs/production5/d1996/54fdb73f-eee5-4612-a3e7-6fc7ed2f7bee.jpg?impolicy=fcrop&w=800&h=533&q=medium" alt = "australia"></img>
+   </div>
+   <div className = "grid-item1">
+    <img src = "https://www.kids-world-travel-guide.com/images/santiagodechile-2.jpg" alt = "Chili"></img>
+   </div>
+   <div className = "grid-item1">
+    <img src = "https://a.cdn-hotels.com/gdcs/production159/d204/01ae3fa0-c55c-11e8-9739-0242ac110006.jpg" alt = "Canada"></img>
+  </div>
+  <div className = "grid-item1">
+      <img src = "https://i.natgeofe.com/n/6c02ad5a-977b-4f12-b9c0-02ffb0736e07/metropolitan-cathedral-zocalo-mexico-city_16x9.JPG " alt= "Mexico"></img>
+      </div>
+      <div className = "grid-item1">
+    <img src = "https://www.state.gov/wp-content/uploads/2018/11/France-1980x1406.jpg" alt ="France"></img>
+    </div>
+      
+    <div className = "grid-item1">
+      <div class = "loginformcontainer1">
+      <div className="auth-form-container">
             <h2>Login</h2>
         <form className="login-form" onSubmit={handleSubmit}>
             <label htmlFor="email">Email</label><br></br>
@@ -21,6 +67,16 @@ import React, { useState } from "react";
         <button className="link-btn" onClick={()=> props.onFormSwitch('register')}>Don't have an account? Register here</button>
         </form>
         </div>
+
+      
+   </div>
+   </div>
+  
+    
+   
+    </div>
+   </div>
+        
     )
 }
 export default Login;
