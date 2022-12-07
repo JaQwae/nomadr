dcf4691327651f401982e47ac40e8d08e5d7294f
JaQwae <105896967+JaQwae@users.noreply.github.com>
2022-12-06 21:02:56 -0600

    changes to connection.js

--- server/config/connection.js
+++ server/config/connection.js
@@ -1,12 +1,11 @@
 const mongoose = require("mongoose");
 
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/nomadr', error =>  {
-  if (error ) throw error
-  // useNewUrlParser: true,
-  // useUnifiedTopology: true,
-  // useCreateIndex: true,
-  // useFindAndModify: false,
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/nomadr', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false,
 });
 
 // Node will look for this environment variable and if it exists, it will use it. Otherwise, it will assume that you are running this application locally
